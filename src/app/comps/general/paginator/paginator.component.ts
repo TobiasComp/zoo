724f6edd8e8bf3e1309ac378335cdcde5fae6bab
TobiasComp <tuvia1234@gmail.com>
2019-11-11 16:08:24 +0200

    pushing json file

--- src/app/comps/general/paginator/paginator.component.ts
+++ src/app/comps/general/paginator/paginator.component.ts
@@ -17,13 +17,5 @@ export class PaginatorComponent implements OnInit {
   moveItem(changeIndex: number){
     this.moveItemEvent.emit(changeIndex);
   }
-  // moveItem(changeIndex:number){
-  //   console.log("moveItem invoked",changeIndex,"the index is",this.birdService.currentIndex);
-  //   if (this.birdService.currentIndex==0 && changeIndex==-1)
-  //     this.birdService.currentIndex = 4;
-  //   else {
-  //     this.birdService.currentIndex = (changeIndex + this.birdService.currentIndex) % 5;
-  //     this.birdService.currentBird = this.birdService.birds[this.birdService.currentIndex];
-  //   }
-  // }
+ 
 }
--- src/app/services/wild.service.ts
+++ src/app/services/wild.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Wild } from '../models/wilds';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WildService {
 
-  constructor() { }
+  wilds: Wild[] = [];
+  currentWildIndex = 0;
+  currentWild:Wild;
+
+  constructor(private http:HttpClient) { 
+    console.log("construct WildService");
+    http.get('https://raw.githubusercontent.com/TobiasComp/zoo/master/src/assets/wilds.json')
+      .subscribe(data =>{
+      this.wilds = data as Wild[];
+      this.currentWild = this.wilds[0];
+    })
+  }
 }
